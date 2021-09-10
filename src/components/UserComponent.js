750c177eae1fc5f9367a95866f050641f1e09c55
Deepesh Subedi <dsubedi1130@gmail.com>
2021-09-10 18:56:50 -0500

    Creating constructors and passing users in the state oif the class

---
+++ src/components/UserComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import userService from "../services/userService";
+
+class UserComponent extends React.Component{
+
+    constructor(){
+        this.state={
+            users:[]
+        }
+    }
+
+}
+export default UserComponent;
