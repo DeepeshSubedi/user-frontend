e797f8fb851cffc7bb339b2f8d2cb0a02496c594
Deepesh Subedi <dsubedi1130@gmail.com>
2021-09-10 19:06:12 -0500

    Creating Component did mount to load data from back end and  created Table to show data

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -8,6 +8,33 @@ class UserComponent extends React.Component{
             users:[]
         }
     }
+    componentDidMount(){
+        userService.getUsers().then((response)=>{
+            this.setState({users: response.data})
+
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1 className="text-center">Users List</h1>
+                <table className="table table-striped">
+                    <thead>
+                     <tr>
+                         <td>User ID</td>
+                         <td>User First Name</td>
+                         <td>User Last Name</td>
+                         <td>User Email</td>
+                         </tr>   
+                    </thead>
+                    
+
+                </table>
+
+            </div>
+        )
+    }
 
 }
 export default UserComponent;
